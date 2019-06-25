a1d8fade011eff4c33f6337313db5f9762ba9ca9
Julia Paulasaari <julia@paulasaari.com>
2019-06-25 01:38:11 +0000

    added the function returnung head of the array

---
+++ head.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+    
+};
+  
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const head = function(item) {
+  return item[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]));
+assertEqual(head([]));
+
+
+
+
+
+
+
