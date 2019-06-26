7afb949c886f9f315bb27c10b82d72359e955480
Julia Paulasaari <julia@paulasaari.com>
2019-06-26 04:56:46 +0000

    Created eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+    
+};
+
+const eqArrays = function(arr1, arr2) {
+  let output = true;
+  arr1.forEach(function(element, index) {
+    if (element !== arr2[index]) {
+      output = false;
+    }
+  });
+  return output;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
