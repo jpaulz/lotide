13d0623ffe5eae2fc24c47118a33878f4f6ccfbb
Julia Paulasaari <julia@paulasaari.com>
2019-07-02 00:45:14 +0000

    refractored the code

--- tail.js
+++ tail.js
@@ -1,11 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
     
-};
+// };
+const assertEqual = require('./assertEqual');
 const tail = function(item) {
   return item.slice(1);
 };
