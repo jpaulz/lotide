4a0b9447c53834dc3620ad7c8d7a5d3d6aa4f5fb
Julia Paulasaari <julia@paulasaari.com>
2019-07-02 02:11:49 +0000

    added refactored code

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+
+
+const eqArrays = require('../eqArrays.js');
+const assertArrayEqual = require('../assertArraysEqual');
+
+
+assertArrayEqual(["hduf"], ["hgir8"]);
+assertArrayEqual(["hduf"], ["hduf"]);
