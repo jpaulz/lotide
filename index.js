1e018b10733803d4b2eaf325202d1d47210d1efc
Julia Paulasaari <julia@paulasaari.com>
2019-07-02 21:37:15 +0000

    exported all function in an object

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const assertArrayEqual = require('./assertArrayEqual');
+const eqArrays = require('./eqArrays');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  assertArrayEqual: assertArrayEqual,
+  eqArrays: eqArrays
+};
