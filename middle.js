489821cabb4067c275fdeac18a59450b2b38d559
Julia Paulasaari <julia@paulasaari.com>
2019-07-02 03:04:08 +0000

    refactored code for middle

--- middle.js
+++ middle.js
@@ -23,8 +23,4 @@ const middle = function(array) {
   }
   
 };
-assertArrayEqual(middle([1, 2, 3, 4]), [2, 3]);// => [2, 3]
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
-
-assertArrayEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+module.exports = middle;
