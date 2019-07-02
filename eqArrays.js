4f29d6cb4c9b82200bc450f891a34b603eb2d305
Julia Paulasaari <julia@paulasaari.com>
2019-07-02 01:31:56 +0000

    refactored code

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
-  }
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+//   } else {
+//     console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+//   }
     
-};
+// };
 
 const eqArrays = function(arr1, arr2) {
   let output = true;
@@ -17,10 +17,4 @@ const eqArrays = function(arr1, arr2) {
   return output;
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
